bf3c3972d7d402132dc184d157598f30d0e16bbf
IrinaPeshko <privet-kira1999@mail.ru>
2023-05-08 12:44:30 +0300

    feat: create task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const copyFolder = path.join(__dirname, "files-copy");
+const originFolder = path.join(__dirname, "files");
 
+function copyDirectory() {
+  fs.promises
+    .access(copyFolder)
+    .catch(() => fs.promises.mkdir(copyFolder))
+    .then(() => fs.promises.readdir(copyFolder))
+    .then((files) => {
+      for (file of files) {
+        const interCopyFolder = path.join(copyFolder, file);
+        fs.stat(interCopyFolder, (err, stats) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (stats.isFile()) {
+            fs.unlink(interCopyFolder, (err) => {
+              if (err) {
+                throw err;
+              }
+              console.log(`файл ${interCopyFolder} удален`);
+            });
+          }
+        });
+      }
+    })
+    .then(() => fs.promises.readdir(originFolder))
+    .then((files) => {
+      for (file of files) {
+        let originFile = path.join(originFolder, file);
+        let copyFile = path.join(copyFolder, file);
+
+        fs.copyFile(originFile, copyFile, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(`Файл ${file} успешно скопирован в ${copyFile}!`);
+          }
+        });
+      }
+    })
+}
+
+copyDirectory();
